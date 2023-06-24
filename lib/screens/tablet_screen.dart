eaf632cc1ca39f49db66a7159a2baaabd66d8128
Waiting <waitinghc@foxmail.com>
2023-06-24 21:25:43 +0800

    更新macOS图标

--- lib/screens/tablet_screen.dart
+++ lib/screens/tablet_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_chat/services/local_storage_service.dart';
-import 'package:flutter_chat/widgets/empty_chat_widget.dart';
+import 'package:Chatbot/services/local_storage_service.dart';
+import 'package:Chatbot/widgets/empty_chat_widget.dart';
 
 import '../bloc/chat_bloc.dart';
 import '../bloc/chat_event.dart';
--- lib/widgets/conversation_list_widget.dart
+++ lib/widgets/conversation_list_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_chat/services/local_storage_service.dart';
+import 'package:Chatbot/services/local_storage_service.dart';
 
 import '../bloc/blocs.dart';
 import '../models/models.dart';
